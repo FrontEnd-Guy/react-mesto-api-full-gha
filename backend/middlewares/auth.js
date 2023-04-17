0cf5018652f41e296fe03f89a9c67063ed508edd
Pavel Zakharov <trade93@gmail.com>
2023-04-17 16:09:26 -0500

    token

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,18 +1,39 @@
 const jwt = require('jsonwebtoken');
 const { UnauthorizedError } = require('../errors');
 
-const auth = (req, res, next) => {
-  const token = req.cookies.jwt;
-  if (!token) {
-    return next(new UnauthorizedError('Необходима авторизация'));
-  }
-  try {
-    const payload = jwt.verify(token, 'secret-key');
-    req.user = payload;
-    return next();
-  } catch (error) {
-    return next(new UnauthorizedError('Необходима авторизация'));
-  }
-};
+// const auth = (req, res, next) => {
+//   const token = req.cookies.jwt;
+//   if (!token) {
+//     return next(new UnauthorizedError('Необходима авторизация'));
+//   }
+//   try {
+//     const payload = jwt.verify(token, 'secret-key');
+//     req.user = payload;
+//     return next();
+//   } catch (error) {
+//     return next(new UnauthorizedError('Необходима авторизация'));
+//   }
+// };
 
-module.exports = auth;
+// module.exports = auth;
+
+module.exports.auth = (req, res, next) => {
+  const { authorization } = req.headers;
+
+  if (!authorization || !authorization.startsWith('Bearer ')) {
+    throw new UnauthorizedError('Необходима авторизация');
+  }
+
+  const token = authorization.replace('Bearer ', '');
+  let payload;
+
+  try {
+    payload = jwt.verify(token, 'secret-key');
+  } catch (err) {
+    next(new UnauthorizedError('Необходима авторизация'));
+  }
+
+  req.user = payload;
+
+  next();
+};
